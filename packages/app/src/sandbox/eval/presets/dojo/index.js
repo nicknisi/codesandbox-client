0cb3a66d40d4887dec61732f1377a81d2541b974
Nick Nisi <nick@nisi.org>
2019-10-27 11:44:57 -0500

    [WIP] evaluate dojorc and inject cldr data into sandbox

--- packages/app/src/sandbox/eval/presets/dojo/index.js
+++ packages/app/src/sandbox/eval/presets/dojo/index.js
@@ -7,6 +7,46 @@ import jsonTranspiler from '../../transpilers/json';
 import stylesTranspiler from '../../transpilers/style';
 import dojoStylesTranspiler from './transpilers/style';
 import babelTranspiler from '../../transpilers/babel';
+// import { Module } from '../../entities/module';
+import TranspiledModule from '../../transpiled-module';
+
+async function evaluateConfig(manager) {
+  const { dojo } = manager.configurations;
+  if (dojo && dojo.parsed && dojo.parsed['build-app']) {
+    const config = dojo.parsed['build-app'];
+    const {
+      locale: defaultLocale = 'en',
+      supportedLocales = [],
+      cldrPaths = [],
+    } = config;
+    const locales = [defaultLocale, ...supportedLocales];
+
+    const code = `
+      (() => {
+      const { deepAssign } = require('@dojo/framework/core/util');
+      const i18n = require('@dojo/framework/i18n/i18n');
+      const loadCldrData = require('@dojo/framework/i18n/cldr/load').default;
+      const systemLocale = i18n.systemLocale;
+
+      const userLocale = systemLocale.replace(/^([a-z]{2}).*/i, '$1');
+      const locales = ${JSON.stringify(locales)};
+      const cldrPaths = ${JSON.stringify(cldrPaths)};
+      const cldrData = cldrPaths
+        .map(url => locales.map(locale => url.replace('{locale}', locale)))
+        .reduce((left, right) => left.concat(right), [])
+        .map(mid => require(mid))
+        .reduce((cldrData, source) => deepAssign(cldrData, source), Object.create(null));
+      loadCldrData(JSON.stringify(cldrData));
+      })();
+    `;
+
+    const module = { code, path: 'build-app.js' };
+    const tModule = new TranspiledModule(module);
+    await tModule.transpile(manager);
+    tModule.setIsEntry(true);
+    tModule.evaluate(manager);
+  }
+}
 
 export default function initialize() {
   const preset = new Preset(
@@ -16,7 +56,7 @@ export default function initialize() {
     {
       setup: async manager => {
         const stylesPath = absolute(join('src', 'main.css'));
-
+        await evaluateConfig(manager);
         try {
           const tModule = await manager.resolveTranspiledModuleAsync(
             stylesPath,
